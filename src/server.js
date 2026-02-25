2e78e102df93ac4b357b02e6b77f86dc3c3c4948
joyajala7-code <joyajala7@gmail.com>
2026-02-25 11:22:00 +0100

    updated localhost

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https//localhost:5174",
+    origin: "http://localhost:5173",
     method: ["GET", "POST", "DELETE", "PUT"],
     credential:true
 }
